611f5c7dd878a795dbed26f406ea17f79235d75f
Ethan <eribe403@gmail.com>
2017-11-16 11:51:54 -0800

    t

---
+++ RIB_3/contact/views.py
@@ -0,0 +1,26 @@
+from django.core.mail import send_mail, BadHeaderError, EmailMessage
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+def contact(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+          full_name = form.cleaned_data['full_name']
+          subject = form.cleaned_data['subject']
+          from_email = form.cleaned_data['from_email']
+          message = form.cleaned_data['message']
+          try:
+            final_form = "Full Name: " + full_name + "\n\n" + "From: " + from_email + "\n\n" + message
+            send_mail(subject, final_form, 'eribe403@gmail.com', ['eribe403@gmail.com'], fail_silently=False)
+
+          except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+          return redirect('success')
+    return render(request, "contact/contact.html", {'form': form})
+
+def success(request):
+    return HttpResponse('Success! Thank you for your message.')
--- RIB_3/info/urls.py
+++ RIB_3/info/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^about/$', views.about, name='about'),
-	# url(r'^contact/', include('contact.urls')),
+	url(r'^contact/', include('contact.urls')),
 	url(r'^services/$', views.services, name='services'),
 	url(r'^resources/$', views.resources, name='resources')
 ]
